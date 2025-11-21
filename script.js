a30aea464bf98c8b79b697a1594db93cf2c236e0
Sonya0904 <soph.zolotaryova01@gmail.com>
2025-11-21 21:42:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let name = "Sonya";
+let city = "Minsk";
+let hobby = "read";
+
+console.log(`Hello! My name is ${name} I from ${city}, I like ${hobby}.`);
+
+name = "Nastya";
+city = "Minsk";
+hobby = "knitting";
+
+console.log(`Hello! My name is ${name} I from ${city}, I like ${hobby}.`);
+
+let object = "table";
+let number = 3;
+let thereIs = true;
+
+let isThereIs = thereIs ? "open" : "close";
+
+console.log(object, number, isThereIs);
+
+let planetName = "Earth";
+let planetPeople = 8000000000;
+let planetAtmosphear = true;
+
+let isPlanetAtmosphear = planetAtmosphear ? "there is" : "there is no";
+
+console.log(`The ${planetName} has ${planetPeople} people, atmasphear: ${isPlanetAtmosphear}`);
+
+let steps = 0;
+
+steps += 5;
+console.log(steps);
+
+let box;
+let emptyBox = null;
+console.log(box,emptyBox);
+
+let colors = ["red","green","blue"];
+
+console.log(colors[0], colors[colors.length-1]);
+
+let itemType = "boll";
+let itemWeight = 20;
+let isMagic = true;
+
+let isItemMagic = isMagic? "yes":"no";
+
+console.log(`item: ${itemType}, weight: ${itemWeight}, magic: ${isItemMagic}`);
